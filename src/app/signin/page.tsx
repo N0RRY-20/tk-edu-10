ca7383a0274c3b01b8490f8e1812317c6f9cc6ec
N0RRY-20 <orywu1994@gmail.com>
2025-10-30 19:52:09 +0700

    feat(auth): implement react-hook-form for login and add form components
    - Add react-hook-form, @hookform/resolvers, and zod dependencies
    - Remove Apple login option and update login form UI
    - Create new form components for form handling and validation
    - Update school icon and dynamic school name from env

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,5 +1,5 @@
 import { LoginForm } from "@/components/auth/login-form";
-import { GalleryVerticalEnd } from "lucide-react";
+import { School } from "lucide-react";
 
 export default function SigninPage() {
   return (
@@ -7,9 +7,9 @@ export default function SigninPage() {
       <div className="flex w-full max-w-sm flex-col gap-6">
         <a href="#" className="flex items-center gap-2 self-center font-medium">
           <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
-            <GalleryVerticalEnd className="size-4" />
+            <School className="size-4" />
           </div>
-          Acme Inc.
+          {process.env.NAMA_TK}
         </a>
         <LoginForm />
       </div>
